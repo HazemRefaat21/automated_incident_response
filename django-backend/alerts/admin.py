20d4e914fe0bbc0c8835a3852ea8866413ece323
hazem <email@example.com>
2026-05-01 03:17:52 +0300

    feat: register ResponseAction, Alert, IPProfile, and DetectionRule models in the admin interface

--- django-backend/alerts/admin.py
+++ django-backend/alerts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Alert, IPProfile, DetectionRule
 
-# Register your models here.
+admin.site.register(Alert)
+admin.site.register(IPProfile)
+admin.site.register(DetectionRule)
